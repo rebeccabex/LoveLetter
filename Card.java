84ec372ef3615b1d68477b449f59e0f22384dc9d
Rebecca Burton <rebeccabexb@gmail.com>
2017-12-17 14:28:47 +0000

    Added methods to Card

--- Card.java
+++ Card.java
@@ -11,6 +11,28 @@ public Card {
 		this.rank = rank;
 		
 	}
+	
+	public int effect(int player) {
+		return effect(player, 0);
+	}
+	
+	public int effect(int player, int target) {
+		
+		int discard = 0;
+		
+		switch (role) {
+			case: "princess":
+				princessEffect(player);
+			
+		}
+		
+		return discard;
+		
+	}
+	
+	public int princessEffect(player) {
+		return player;
+	}
 
 }
 
